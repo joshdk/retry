ccfd5dc39883644231d10142a60b2a3e5ad2f70e
Josh Komoroske <jdkomo@gmail.com>
2019-11-26 08:02:29 -0800

    Flag for printing the version

--- main.go
+++ main.go
@@ -5,12 +5,18 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/joshdk/retry/retry"
 )
+
+// version is used to hold the version string. Will be replaced at go build
+// time with -ldflags.
+var version = "development"
 
 func main() {
 	if err := mainCmd(); err != nil {
@@ -21,13 +27,26 @@ func main() {
 
 func mainCmd() error {
 	var spec retry.Spec
-	flag.IntVar(&spec.Attempts, "attempts", 0, "")
-	flag.BoolVar(&spec.Backoff, "backoff", false, "")
-	flag.IntVar(&spec.Consecutive, "consecutive", 0, "")
-	flag.DurationVar(&spec.TotalTime, "max-time", 0, "")
-	flag.DurationVar(&spec.Sleep, "sleep", 0, "")
-	flag.DurationVar(&spec.TaskTime, "task-time", 0, "")
+	var versionFlag bool
+	flag.IntVar(&spec.Attempts, "attempts", 3, "maximum number of attempts")
+	flag.BoolVar(&spec.Backoff, "backoff", false, "use exponential backoff when sleeping")
+	flag.IntVar(&spec.Consecutive, "consecutive", 0, "required number of back to back successes")
+	flag.DurationVar(&spec.TotalTime, "max-time", time.Minute, "maximum total time")
+	flag.DurationVar(&spec.Sleep, "sleep", 5*time.Second, "time to sleep between attempts")
+	flag.DurationVar(&spec.TaskTime, "task-time", 0, "maximum time for a single attempt")
+	flag.BoolVar(&versionFlag, "version", false, fmt.Sprintf("print the version %q and exit", version))
 	flag.Parse()
+
+	// If the version flag (-version) was given, print the version and exit.
+	if versionFlag {
+		fmt.Println(version)
+		return nil
+	}
+
+	// If no arguments were given, there's nothing to do.
+	if flag.NArg() == 0 {
+		return errors.New("no command given")
+	}
 
 	task := retry.NewExecTask(flag.Args()[0], flag.Args()[1:]...)
 
